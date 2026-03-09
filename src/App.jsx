9cfc25e7b146940505261e0f08161764b430785f
Gyo-moon Gu <mooni@Gyo-moonui-MacBookPro.local>
2026-03-09 23:24:16 +0900

    jsx base commit.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import { useState } from "react";
 
 const mockData = [
   {
@@ -26,11 +27,13 @@ const mockData = [
 ];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
       <Editor />
-      <List />
+      <List todos={todos} />
     </div>
   );
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,12 @@
 import "./Editor.css";
 
 const Editor = () => {
-  return <></>;
+  return (
+    <div className="Editor">
+      <input placeholder="새로운 ToDo..." />
+      <button>추가</button>
+    </div>
+  );
 };
 
 export default Editor;
