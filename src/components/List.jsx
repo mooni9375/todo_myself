b810fe74066c675e6f7449305733f2aec5f2fbe6
Gyo-moon Gu <mooni@Gyo-moonui-MacBookPro.local>
2026-03-14 13:20:25 +0900

    List → TodoItem todos 전달 기능 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,9 +8,10 @@ const List = ({ todos }) => {
       <h4>Todo List 🌱</h4>
       <input placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {/* 리스트 형태로 랜더링 된 컴포넌트들이나 요소들을 서로 구분할 때 'key' prop으로 구분 */}
+        {todos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
