9d80ad942f3525cf3784fae959790b7957104ba7
Gyo-moon Gu <mooni@Gyo-moonui-MacBookPro.local>
2026-03-16 23:16:37 +0900

    TodoItem Checkbox 수정 기능 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,14 +1,14 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = ({ todos }) => {
+const List = ({ todos, onUpdate }) => {
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
       <input placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
         {todos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} />;
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />;
         })}
       </div>
     </div>
