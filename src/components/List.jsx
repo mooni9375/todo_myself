a97631b86b4bab4f7aa1a5e713239c1a3116f557
Gyo-moon Gu <mooni@Gyo-moonui-MacBookPro.local>
2026-03-26 00:02:45 +0900

    List 검색 기능 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,34 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value.toLowerCase());
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) => todo.content.toLowerCase().includes(search));
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        {todos.map((todo) => {
-          return (
-            <TodoItem
-              key={todo.id}
-              {...todo}
-              onUpdate={onUpdate}
-              onDelete={onDelete}
-            />
-          );
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
         })}
       </div>
     </div>
