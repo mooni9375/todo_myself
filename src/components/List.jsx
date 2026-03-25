22ed132f6a7f49dd2951e959a39c3a7de2412bdb
Gyo-moon Gu <mooni@Gyo-moonui-MacBookPro.local>
2026-03-26 00:23:57 +0900

    useMemo 적용

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -13,14 +13,51 @@ const List = ({ todos, onUpdate, onDelete }) => {
     if (search === "") {
       return todos;
     }
-    return todos.filter((todo) => todo.content.toLowerCase().includes(search));
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()),
+    );
   };
 
   const filteredTodos = getFilteredData();
 
+  // search가 변경될 때마다 리랜더링 되면서 호출 됨 (search는 useState에 의해 변경이 감지될 때마다 리랜더링)
+  // const getAnalyzedData = () => {
+  //   console.log("getAnalyzedDate is called...");
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((todo) => todo.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount,
+  //   };
+  // };
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  // 인수 : (callback, array)
+  // array는 deps임. deps에 포함된 데이터의 변경이 있을 때만 callback을 호출
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedDate is called...");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount}</div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch}
@@ -28,7 +65,14 @@ const List = ({ todos, onUpdate, onDelete }) => {
       />
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} />;
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
         })}
       </div>
     </div>
